df4d0118c3f7a9ebe9aa1e199f1ba382a4c98168
dghuang <davidgao7@live.com>
2016-03-07 16:52:47 -0500

    Added in config file

---
+++ Node/config.js
@@ -0,0 +1,8 @@
+// Private Variable configuration
+// Access code is private for now, you'd have to add your own access code in
+module.exports = function() {
+	var config = {
+		ACCESS_CODE:''
+	};
+	return config;
+};
